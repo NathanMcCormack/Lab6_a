3dbce2fd23065b065662dd791fe5845ea604f0e9
NathanMcCormack <118372005+NathanMcCormack@users.noreply.github.com>
2025-11-28 11:04:23 +0000

    lab 6 steps

--- app/main.py
+++ app/main.py
@@ -7,6 +7,13 @@ from app.database import engine, SessionLocal
 from app.models import Base, UserDB
 from app.schemas import UserCreate, UserRead
 
-app = FastAPI()
+app = FastAPI(title="Service A - Greeting API")
+
 Base.metadata.create_all(bind=engine)
 
+@app.get("/health")
+def health():
+return {"status": "ok"}
+@app.get("/api/greet/{name}")
+def greet(name: str):
+return {"message": f"Hello, {name} from Service A!"}
